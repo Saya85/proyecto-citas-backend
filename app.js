e52a40deed8473297a5c5796ef12de6ece60a3d3
Saya85 <saya.casinovidal@gmail.com>
2022-02-25 20:24:02 +0100

    add cors and minor fix app.js instnce cors

--- app.js
+++ app.js
@@ -9,19 +9,22 @@ var usersRouter = require('./routes/users');
 var mascotasRouter = require('./routes/mascotas');
 var veterinariosRouter = require('./routes/veterinarios');
 var citasRouter = require('./routes/citas');
-
+var cors = require('cors')
 var app = express();
+const PORT = process.env.PORT || 3000; 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mascotas', mascotasRouter);
 app.use('/veterinarios', veterinariosRouter);
 app.use('/citas', citasRouter);
+
 
 module.exports = app;
